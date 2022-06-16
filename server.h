e0975cd16fe0f6e65a74ca599ec2988705ae33ca
alon-exp <52732705+alon-exp@users.noreply.github.com>
2022-06-16 12:01:21 +0800

    Declare static function

--- server.h
+++ server.h
@@ -1,16 +1,16 @@
 #ifndef __SERVER_H__
 #define __SERVER_H__
 
-void method_reply_cb(struct event_data *client_event);
-void socks_reply_cb(struct event_data *client_event);
-void conn_relay(struct event_data *client_event, struct socks_request *socks_request);
-void bind_relay(struct event_data *client_event, struct socks_request *socks_request);
-void udp_relay(struct event_data *client_event, struct socks_request *socks_request);
-void not_support(struct event_data *client_event, struct socks_request *socks_request, uint8_t rep);
-void forword_data_cb(int dst_fd, int src_fd);
-void tcp_relay_cb(struct event_data* event);
-void udp_relay_cb(struct event_data* event);
-void auth_cb(struct event_data* client_event);
-void accept_cb(struct event_data *server_event);
+static void method_reply_cb(struct event_data *client_event);
+static void socks_reply_cb(struct event_data *client_event);
+static void conn_relay(struct event_data *client_event, struct socks_request *socks_request, in_addr_t conn_addr);
+static void bind_relay(struct event_data *client_event, struct socks_request *socks_request);
+static void udp_relay(struct event_data *client_event, struct socks_request *socks_request);
+static void not_support(struct event_data *client_event, struct socks_request *socks_request, uint8_t rep);
+static void forword_data_cb(int dst_fd, int src_fd);
+static void tcp_relay_cb(struct event_data *event);
+static void udp_relay_cb(struct event_data *event);
+static void auth_cb(struct event_data *client_event);
+static void accept_cb(struct event_data *server_event);
 
 #endif
