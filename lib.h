49acf04a8939a1cddf45b96f509cbf40aea927f6
alon-exp <52732705+alon-exp@users.noreply.github.com>
2022-06-16 11:50:59 +0800

    Add dns_cache structure

--- lib.h
+++ lib.h
@@ -9,19 +9,33 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <ares.h>
 #include <sys/epoll.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <uthash.h>
 
 #define EPOLL_MAX_EVENTS 64
 
 struct event_data
 {
     int fd;
+    int type;
     struct sockaddr_in addr;
     void (*cb)(struct event_data *event_data);
     struct event_data *to;
 };
+
+struct dns_cache
+{
+    char *domain_name;
+    uint32_t ipv4;
+    uint8_t ipv6[16];
+    UT_hash_handle hh;
+};
+
+#define EVENT_DATA_TYPE_NORMAL 1
+#define EVENT_DATA_TYPE_DNS 2
 
 typedef void (*cb_t)(struct event_data *event_data);
 
